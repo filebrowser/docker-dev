8bcb5c942ed757c44826887add9e394716fb96de
1138-4EB <1138-4EB@users.noreply.github.com>
2018-12-25 05:07:18 +0100

    add workaround for failing GitHub API calls

--- get_deps.sh
+++ get_deps.sh
@@ -18,11 +18,15 @@ chmod +x /go/bin/docker
 
 #-- docker-credential-pass
 
-curl -fsSL $( \
-  curl -s https://api.github.com/repos/docker/docker-credential-helpers/releases/latest \
-  | grep "browser_download_url.*pass-.*-amd64"  \
+PASS_URL="$(curl -s https://api.github.com/repos/docker/docker-credential-helpers/releases/latest \
+  | grep "browser_download_url.*pass-.*-amd64" \
   | cut -d : -f 2,3 \
-  | tr -d \" \
-) | tar xv -C /go/bin
+  | tr -d \")"
+
+if [ "$(echo "$PASS_URL" | cut -c2-6)" != "https" ]; then
+  PASS_URL="https://github.com/docker/docker-credential-helpers/releases/download/v0.6.0/docker-credential-pass-v0.6.0-amd64.tar.gz"
+fi
+
+curl -fsSL "$PASS_URL" | tar xv -C /go/bin
 
 chmod + /go/bin/docker-credential-pass
