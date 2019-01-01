3f69f45423cbb1cb7df1ad4a9ebe8bc8654ed110
1138-4EB <1138-4EB@users.noreply.github.com>
2019-01-01 18:18:26 +0100

    feat: replace gometalinter with golangci-lint

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
-FROM golang:alpine AS base
-
-RUN apk add -U --no-cache git && \
-  go get github.com/alecthomas/gometalinter && \
-  gometalinter --install
-
 FROM golang:alpine
-
-COPY --from=base /go/bin /go/bin
-COPY get_deps.sh ./get_deps.sh
 
 ENV CGO_ENABLED 0
 
-RUN apk --no-cache -U upgrade && apk --no-cache add ca-certificates yarn git curl dos2unix && \
-  chmod +x get_deps.sh && ./get_deps.sh && rm get_deps.sh
+RUN apk --no-cache -U upgrade && apk --no-cache add ca-certificates yarn git curl dos2unix
+
+COPY get_deps.sh ./get_deps.sh
+
+RUN dos2unix get_deps.sh && chmod +x ./get_deps.sh && ./get_deps.sh && rm get_deps.sh
--- get_deps.sh
+++ get_deps.sh
@@ -2,9 +2,13 @@
 
 #-- go.rice
 
+echo "INSTALL go.rice"
+
 go get github.com/GeertJohan/go.rice/rice
 
 #-- goreleaser
+
+echo "INSTALL goreleaser"
 
 curl -sL https://git.io/goreleaser -o /go/bin/goreleaser
 
@@ -12,11 +16,15 @@ chmod +x /go/bin/goreleaser
 
 #-- docker
 
+echo "INSTALL docker CLI"
+
 curl -fsSL https://download.docker.com/linux/static/edge/x86_64/docker-18.05.0-ce.tgz | tar xvz --strip-components=1 docker/docker -C /go/bin
 
 chmod +x /go/bin/docker
 
 #-- docker-credential-pass
+
+echo "INSTALL docker-credential-pass"
 
 PASS_URL="$(curl -s https://api.github.com/repos/docker/docker-credential-helpers/releases/latest \
   | grep "browser_download_url.*pass-.*-amd64" \
@@ -33,3 +41,14 @@ echo "PASS_URL: $PASS_URL"
 curl -fsSL "$PASS_URL" | tar xv -C /go/bin
 
 chmod + /go/bin/docker-credential-pass
+
+#-- golangci-lint
+
+echo "INSTALL golangci-lint"
+
+#GOLANGCI_VER="v1.12.5"
+GOLANGCI_VER="latest"
+
+echo "GOLANGCI_VER: $GOLANGCI_VER"
+
+curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin "$GOLANGCI_VER"
